6a22c885d2949005392f43cdb4ebee2cb4076841
Dillon Parchment <dillonparchmentkool@gmail.com>
2021-01-14 20:17:22 -0500

    Split Vuex Store in Multiple Files

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import shop from '@/api/shop'
-
+import actions from './actions'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -43,44 +42,7 @@ export default new Vuex.Store({
             }
         }
     },
-    actions: { // methods
-        fetchProducts ({commit}) {
-            return new Promise((resolve, reject) => {
-            
-                // make the call & run setProducts mutation 
-            shop.getProducts(products => {
-                commit('setProducts', products)
-                resolve()
-            })  
-            })
-        },
-        addProductToCart({state, getters, commit}, product){
-           if (getters.productsInStock(product)){
-               //find cartItem
-               const cartItem = state.cart.find(item => item.id === product.id)
-               if (!cartItem){
-                   //pushProductToCart
-                   commit('pushProductToCart',product.id)
-               }else{
-                // incrementItemQuantity
-                commit('incrementItemQuantity', cartItem)
-               }
-               commit('decrementProductInventory', product)
-           } 
-        },
-        checkout({state,commit}){
-            shop.buyProducts(
-                state.cart,
-                () => {
-                    commit('emptyCart')
-                    commit('setCheckoutStatus', 'success')
-                },
-                () => {
-                    commit('setCheckoutStatus', 'fail')
-                }
-            )
-        }
-    },
+    actions: actions,
     mutations: { // for setting and updating the state 
         setProducts (state, products) {
             // update products
